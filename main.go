a2067523da455da43615d1fa2780865eb6b106a5
kid <1050109853@qq.com>
2022-03-19 22:22:29 +0800

    fin

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello ci/cd<h1>"))
+		w.Write([]byte("<h1>Hello world!<h1>"))
 	})
 	http.ListenAndServe(":8081", nil)
 }
